738c27a01aa72ea5f4feded595a0267458ce72f7
wkdtmdgnsdev <wkdtmdgnsdev@gmail.com>
2025-03-14 16:39:31 +0900

    3. 브라우저에서 수정함

--- src/com/msa2025/Hello.java
+++ src/com/msa2025/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("안녕하세요~~~");
 		System.out.println("안녕하세요~~~");
+		System.out.println("브라우저에서 수정>> 안녕하세요~~~");
 	}
 
 }
